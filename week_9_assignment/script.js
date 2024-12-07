68b9059599e08d2688c99edcf96a3e0721e1dddb
Jiwon-Jay-Jang <jiwonjang.design@gmail.com>
2024-12-07 12:08:49 -0800

    Add files via upload

--- week_9_assignment/script.js
+++ week_9_assignment/script.js
@@ -1,105 +1,82 @@
+let bgImage;
 let video;
-let classifier;
-let emoji = "🤓";
-
-let _isDrawing = false;
-
 let handPose;
 let hands = [];
-let indexPosX = 0,
-  indexPosY = 0;
-let thumbPosX = 0;
-thumbPosY = 0;
+let indexPosX;
+let indexPosY;
+let thumbPosX;
+let thumbPosY;
 
-function preload() {
+let leftHandImage;
+let rightHandImage;
+let emojis = [];
+let emojiDisplayed = false;
+
+function preload(){
   handPose = ml5.handPose();
-  classifier = ml5.imageClassifier(
-    "https://teachablemachine.withgoogle.com/models/hkRUrdNBv/"
-  );
+  bgImage = loadImage("Frame1.jpg");
+  leftHandImage = loadImage("snowman.png")
+  rightHandImage = loadImage("star.png")
 }
 
-function classifyVideo() {
-  classifier.classify(video, gotResults);
-}
-
-// Callback function for when handPose outputs data
-function gotHands(results) {
-  // save the output to the hands variable
-  hands = results;
-}
-
-function gotResults(results) {
-  if (_isDrawing) {
-    return;
-  }else{
-    console.log ('detecting')
-  }
-  console.log (results)
-  switch (results[0].label) {
-    case "NoObject":
-      classifyVideo();
-      break;
-    case "Coffee":
-      emoji = "☕️";
-      _isDrawing = true;
-      break;
-    case "Water":
-      emoji = "🍺";
-      _isDrawing = true;
-      break;
-  }
-}
-
-function setup() {
+function setup(){
   createCanvas(640, 480);
-  video = createCapture(VIDEO, { flipped: true });
-  video.hide();
-
-  classifyVideo();
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  video.hide()
   handPose.detectStart(video, gotHands);
 }
 
-function draw() {
-  console.log(emoji)
-  //background(255);
+function draw(){
+  background(255);
+  image(bgImage, 0, 0, width, height)
   //image(video, 0, 0, width, height);
-  if (_isDrawing) {
-    if (hands.length > 0) {
-      let hand = hands[0];
-      //console.log(hand);
-
-      let index_tip = hand.index_finger_tip;
-      let thumb_tip = hand.thumb_tip;
-
-      (indexPosX = width - index_tip.x), (indexPosY = index_tip.y);
-      thumbPosX = width - thumb_tip.x;
-      thumbPosY = thumb_tip.y;
-
-      fill(0, 255, 0);
-      circle(indexPosX, indexPosY, 3);
-
-      fill(255, 0, 0);
-      circle(thumbPosX, thumbPosY, 3);
-
-      let d = dist(index_tip.x, index_tip.y, thumb_tip.x, thumb_tip.y);
-
-      if (d <= 30) {
-        drawEmoji();
+  
+  
+  for (let i = 0; i < hands.length; i++) {
+    let hand = hands[i];
+    //console.log(hand);
+    
+    let index_tip = hand.index_finger_tip;
+    let thumb_tip = hand.thumb_tip;
+    
+    indexPosX = width - index_tip.x;
+    indexPosY = index_tip.y;
+    thumbPosX = width - thumb_tip.x;
+    thumbPosY = thumb_tip.y;
+    
+    fill(0,255,0);
+    circle(indexPosX, indexPosY, 5);
+    
+    fill(0,0,255);
+    circle(thumbPosX, thumbPosY, 5);
+    
+    let d = dist(index_tip.x, index_tip.y, thumb_tip.x, thumb_tip.y);
+    
+    if (d <= 20 && !emojiDisplayed) {
+      if (thumb_tip.x < index_tip.x) {
+        emojis.push({ x: indexPosX, y: indexPosY, image: leftHandImage });
+        
+      } else {
+        emojis.push({ x: indexPosX, y: indexPosY, image: rightHandImage });
       }
+      emojiDisplayed = true; 
     }
+    
+    if (d > 20) {
+      emojiDisplayed = false;
+    }
+    }
+  for (let i = 0; i < emojis.length; i++) {
+    drawImage(emojis[i].x, emojis[i].y, emojis[i].image);
   }
 }
 
-function drawEmoji() {
-  let emojiSize = 72;
-  textSize(emojiSize);
-  fill(255);
-
-  text(emoji, indexPosX - emojiSize / 2, indexPosY + emojiSize / 2);
-
-  console.log("emoji is drawed");
-
-  _isDrawing = false;
-  emoji = ' '
-  classifyVideo();
+function drawImage(x, y, img) {
+  imageMode(CENTER);
+  image(img, x, y, 50, 50);
+}
+   
+function gotHands(results) {
+  hands = results;
 }
